ac4dd6debe20f10ac849e070677fc4cac0a06726
SovathChean <sovathchean09@gmail.com>
2022-08-18 13:33:57 +0700

    fixed gradle

--- postservice/postservice/src/main/java/com/example/postservice/PostserviceApplication.java
+++ postservice/postservice/src/main/java/com/example/postservice/PostserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostserviceApplication {
 
 	public static void main(String[] args) {
--- service-registry/src/test/java/com/example/serviceregistry/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/example/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.gateway;
+package com.example.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class ServiceRegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
