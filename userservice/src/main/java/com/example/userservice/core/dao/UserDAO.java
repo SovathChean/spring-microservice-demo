d88120fbad3e0da3aaba6a06d9793c32b093fb61
SovathChean <sovathchean09@gmail.com>
2022-08-19 16:08:56 +0700

    xml query

--- userservice/src/main/java/com/example/userservice/core/dao/UserDAO.java
+++ userservice/src/main/java/com/example/userservice/core/dao/UserDAO.java
@@ -2,10 +2,7 @@ package com.example.userservice.core.dao;
 
 import com.example.userservice.core.dto.UserDTO;
 import com.example.userservice.core.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.GeneratedValue;
@@ -14,10 +11,6 @@ import javax.persistence.GenerationType;
 @Repository
 @Mapper
 public interface UserDAO {
-    @Insert("INSERT INTO Users(username, phone, password) " +
-            " VALUES ( #{username}, #{phone}, #{password})")
-    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     public int insert(UserDTO userDTO);
-    @Select("Select * From Users Where id=#{userId}")
     public User findUserById(Integer userId);
 }
