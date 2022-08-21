5d5cfb1c736b0b898cebe3928f05226ffa413c42
SovathChean <sovathchean09@gmail.com>
2022-08-21 18:05:21 +0700

    validation exception

--- postservice/postservice/src/main/java/com/example/postservice/web/handler/response/ResponseError.java
+++ postservice/postservice/src/main/java/com/example/postservice/web/handler/response/ResponseError.java
@@ -1,4 +1,17 @@
 package com.example.postservice.web.handler.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseError {
+    private HttpStatus status;
+    private String message;
+    private List<String> error;
 }
